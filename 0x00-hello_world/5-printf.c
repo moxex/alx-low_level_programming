1507a43121e11ae382708493dce5c84d82a69867
Moses <akojimoses@gmail.com>
2022-04-07 09:25:29 -0700

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,15 @@
 #!/bin/bash
 #include <stdio.h>
 
+#include <stdio.h>
+
 /**
- * main - Entry point 
+ * main - Entry point
  *
  * Return: Always 0 (Success)
- * /
- int main(void)
- {
- printf("with proper grammar, but the outcome is a piece of art,\n");
- return (0);
- }
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
