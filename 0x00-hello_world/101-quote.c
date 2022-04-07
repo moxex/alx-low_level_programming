b47251c18ce361b2b64834382733271e8d5d0b83
Moses <akojimoses@gmail.com>
2022-04-07 10:23:56 -0700

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,11 @@
 
 /**
  * main - Entry point 
- * Return: 1
+ * Return: 0
  */
 
 int main(void)
 {
 	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-	return (1);
+	return (0);
 }
